624881bcd130089da9279d3dbe3e82d4862f053d
Training <Training@192.168.252.189>
2020-03-11 15:47:17 +0530

    new update in calc

--- JUnitDemo/src/com/niit/Junit/Calculator.java
+++ JUnitDemo/src/com/niit/Junit/Calculator.java
@@ -1,17 +1,18 @@
 package com.niit.Junit;
 
 public class Calculator 
-{
-	public double add(double a, double b)
+{ 
+	
+	public static double add(double a, double b)
   {
 	  return a+b;
   }
   
-   public double subtract(double a, double b)
+   public static double subtract(double a, double b)
   {
 	  return a-b;
   }
-   public double divide(double a, double b)
+   public static double divide(double a, double b)
    {
  	  if (b==0)
  		  {
@@ -19,8 +20,16 @@ public class Calculator
  		  }
  	  return a/b;
    }
-   public double multiply(double a, double b)
+   public static double multiply(double a, double b)
    {
  	  return a*b;
+   }
+
+public static void main(String[] args)
+   {
+	System.out.println(add(1, 110));
+	System.out.println(subtract(100, 10));
+	System.out.println(divide(220, 110));
+	System.out.println(multiply(13245, 110));
    }
 }
